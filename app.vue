1d04708fd3f2df7726b5e89dc5792a32f654347c
Selwyn <talk@selwyn.cc>
2025-01-27 17:20:12 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    {{ query }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import query from "./index.query.graphql";
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import pluginGraphql from '@rollup/plugin-graphql';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  vite: {
+    plugins: [pluginGraphql()],
+  },
+  $development: {
+    vite: {
+      server: {
+        allowedHosts: true,
+      },
+    },
+  },
+});
