002861483203d58dc30f0cc0835ba2497ee9d069
Selwyn <talk@selwyn.cc>
2025-01-27 17:27:42 +0100

    wip

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    {{ query }}
   </div>
 </template>
-
-<script setup lang="ts">
-import query from "./index.query.graphql";
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>hi</div>
+</template>
+
+<script setup lang="ts">
+import * as query from './index.query.graphql';
+</script>
