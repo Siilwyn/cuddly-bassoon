01344ecb4bebaccd1c43121144e450e65483ea70
Selwyn <talk@selwyn.cc>
2025-01-27 17:33:56 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,14 @@ import pluginGraphql from '@rollup/plugin-graphql';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  routeRules: {
+    '*': { prerender: false },
+  },
   nitro: {
     preset: 'netlify',
+    prerender: {
+      crawlLinks: true,
+    },
   },
   vite: {
     plugins: [pluginGraphql()],
