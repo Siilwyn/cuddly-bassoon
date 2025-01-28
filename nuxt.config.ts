6e6209069736af6d8bb599112f963c57871c6571
Selwyn <talk@selwyn.cc>
2025-01-28 14:07:42 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,33 +3,13 @@ import pluginGraphql from '@rollup/plugin-graphql';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src',
-  devtools: { enabled: true },
-  components: { dirs: [] },
-
-  // Disable confusing @ alias in favor of ~ alias
-  alias: { '@': '' },
-
   routeRules: {
     '*': { prerender: false },
   },
-
   compatibilityDate: '2024-07-26',
   nitro: {
     prerender: {
       crawlLinks: true,
-    },
-  },
-
-  vite: {
-    plugins: [pluginGraphql()],
-  },
-
-  experimental: {
-    componentIslands: true,
-    defaults: {
-      nuxtLink: {
-        trailingSlash: 'append',
-      },
     },
   },
 });
